55a94ff5a44171929af324e713feaf5ef3cc2e15
Sumaia <Somaiya.ghoraibi@gmail.com>
2026-02-20 22:41:16 +0300

    Add sentence

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@
     public static void main(String[] args) {
         System.out.println("Hello, SE4111!");
         System.out.println("Welcome to Software Construction");
+        System.out.println("Guide for Ghala");
+
+
             }
 }
     
