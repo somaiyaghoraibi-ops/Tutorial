6850ac6d61be65feefdfe4bb5b4350b2fcfbc607
Sumaia <Somaiya.ghoraibi@gmail.com>
2026-02-20 23:47:58 +0300

    Moataz

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@
     public static void main(String[] args) {
         System.out.println("Hello, SE4111!");
         System.out.println("Welcome to Software Construction");
-        System.out.println("Guide for Ghala");
+        System.out.println("Guide for Moataz");
 
 
             }
