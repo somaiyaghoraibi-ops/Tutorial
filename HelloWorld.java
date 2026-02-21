63f815199922bda4230c922554b52d5fb0fbb239
Sumaia <Somaiya.ghoraibi@gmail.com>
2026-02-21 11:02:31 +0300

    PSAU3

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,8 @@
     public static void main(String[] args) {
         System.out.println("Hello, SE4111!");
         System.out.println("Welcome to Software Construction");
-        System.out.println("PSAU is my University");
+        System.out.println("PSAU is my University in Saudia");
+
        
             }
 }
